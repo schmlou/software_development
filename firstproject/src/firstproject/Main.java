fdd4ce1a96d189c27053f1540658775d9febc2b1
schmlou <louisschmalisch@gmail.com>
2025-11-03 13:59:08 +0100

    First Project in class

---
+++ firstproject/src/firstproject/Main.java
@@ -0,0 +1,19 @@
+package firstproject;
+
+public class Main {
+//every project needs a main.java file
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 23;
+		double speed = 3.66;
+		boolean isStudent = true;
+		String myText = "This is my biography! ";
+		String myStatement= "I am " + age + " years old!";
+				
+		String biography = myText + myStatement;
+		
+		System.out.println(biography);
+		
+	}
+
+}
