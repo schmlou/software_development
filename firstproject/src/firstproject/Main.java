bd4a156307de5fb6ba35b6f856582509cb45a163
schmlou <louisschmalisch@gmail.com>
2025-11-03 14:30:57 +0100

    final version

--- firstproject/src/firstproject/Main.java
+++ firstproject/src/firstproject/Main.java
@@ -4,7 +4,7 @@ public class Main {
 //every project needs a main.java file
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int age = 23;
+		int age = 13;
 		double speed = 3.66;
 		double secondNumber =  156.01;
 		boolean isStudent = true;
@@ -12,6 +12,23 @@ public class Main {
 		for (int i = 0; i <= 20; i++) {
 			System.out.println("Im a student " + i);
 		}
+		
+		int j = 0;
+		while (j < 20 ) {
+			System.out.println("I'm a student " + j);
+			j = j +1;
+		}
+		
+		
+		if (age >= 18) {
+			System.out.println("You are an adult!");
+		} else {
+			System.out.println("You are a minor!");
+		}
+		
+		
+		
+		
 	}
 
 }
