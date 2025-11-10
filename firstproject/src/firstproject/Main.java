ca8016f80cc82b2edce48f883db19209359fa858
schmlou <louisschmalisch@gmail.com>
2025-11-10 13:57:56 +0100

    average in array

--- firstproject/src/firstproject/Main.java
+++ firstproject/src/firstproject/Main.java
@@ -1,10 +1,12 @@
 package firstproject;
 
+import java.util.Arrays;
+
 public class Main {
 //every project needs a main.java file
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int age = 13;
+		/*int age = 13;
 		double speed = 3.66;
 		double secondNumber =  156.01;
 		boolean isStudent = true;
@@ -24,10 +26,26 @@ public class Main {
 			System.out.println("You are an adult!");
 		} else {
 			System.out.println("You are a minor!");
+		}*/
+		
+		int[] grades = {10, 5, 9, 7};
+		//int myGrades = grades[0];
+		
+		//System.out.println(Arrays.toString(grades));
+		//System.out.println(grades.length);
+		
+		for (int i = 0; i < grades.length; i++) {
+			//System.out.println(grades[i]);
 		}
 		
+		double[] numbers = {9.2, 3.3, 4.9, 6.9, 5.1};
+		double sum = 0;
 		
-		
+		for (int i = 0; i < numbers.length; i++) {
+			sum = sum + numbers[i];
+		}
+		double average = sum/numbers.length;
+		System.out.println(average);
 	}
 
 }
